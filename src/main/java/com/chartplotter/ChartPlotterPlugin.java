d7d1a0974301bf6bc10d72fc237d045584ab1490
Dazuz <87311136+Dazuzi@users.noreply.github.com>
2026-06-05 12:22:38 +0300

    Align @PluginDescriptor description and tags with runelite-plugin.properties

--- src/main/java/com/chartplotter/ChartPlotterPlugin.java
+++ src/main/java/com/chartplotter/ChartPlotterPlugin.java
@@ -1,31 +1,28 @@
 package com.chartplotter;
+
 import com.chartplotter.route.ChartPlotterRoute;
 import com.chartplotter.route.ChartPlotterRoutes;
 import com.chartplotter.runtime.ChartPlotterRuntime;
 import com.chartplotter.runtime.ChartPlotterSailing;
 import com.google.inject.Provides;
-import javax.inject.Inject;
-import net.runelite.api.events.FocusChanged;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.GameTick;
-import net.runelite.api.events.MenuOptionClicked;
-import net.runelite.api.events.MenuOpened;
-import net.runelite.api.events.VarbitChanged;
-import net.runelite.api.events.WorldViewLoaded;
-import net.runelite.api.coords.LocalPoint;
 import net.runelite.api.Point;
 import net.runelite.api.WorldEntity;
 import net.runelite.api.WorldView;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.events.*;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+
+import javax.inject.Inject;
+
 @PluginDescriptor(
 	name = "Chart Plotter",
-	description = "A Chart Plotter helper too to assist you with all your sailing chart plotting needs.",
+	description = "Displays current course, projected sailing path, and destination routes in the world, minimap, and world map.",
 	configName = "chartplotter",
-	tags = {"sailing","sail","heading","navigation","chart","plotter"}
+	tags = {"sailing","sail","path","pathing","plotting","plot","chart","charting","navigation","navigating","nav","map","route","routing","course","ship","boat","minimap","worldmap","destination"}
 )
 public class ChartPlotterPlugin extends Plugin {
 	@Inject private ChartPlotterRuntime runtime;
